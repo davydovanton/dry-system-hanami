4906603bc6041f5be72ee45799bb6db6dbc1e2c5
Gustavo Caso <gustavocaso@gmail.com>
2018-03-30 11:40:29 +0200

    Use Dir.glob to get all ruby files

--- lib/dry/system/hanami/resolver.rb
+++ lib/dry/system/hanami/resolver.rb
@@ -6,29 +6,28 @@ module Dry
 
         def register_folder!(folder, resolver: ->(k) { k.new })
           all_files_in_folder(folder).each do |file|
-            register_name = file.sub("#{PROJECT_NAME}/", '').gsub('/', '.').gsub(/_repository\z/, '')
+            register_name = file.sub("lib/#{PROJECT_NAME}/", '').gsub('/', '.').gsub(/_repository\z/, '')
             register(register_name, memoize: true) { load! file, resolver: resolver }
           end
         end
 
         def all_files_in_folder(folder)
           Dir
-            .entries("#{::Hanami.root}/lib/#{folder}")
-            .select {|f| !File.directory? f}
-            .map! { |file_name| "#{folder}/#{file_name.sub!('.rb', '')}" }
+            .glob("lib/#{folder}/**/*.rb")
+            .map! { |file_name| "#{file_name.sub!('.rb', '')}" }
         end
 
         def load!(path, resolver: ->(k) { k.new })
           load_file!(path)
 
-          unnecessary_part = path[/repositories/] ? "#{PROJECT_NAME}/repositories" : "#{PROJECT_NAME}/"
+          unnecessary_part = path[/repositories/] ? "lib/#{PROJECT_NAME}/repositories" : "lib/#{PROJECT_NAME}/"
           right_path = path.sub(unnecessary_part, '')
 
           resolver.call(Object.const_get(Inflecto.classify(right_path).sub('Statu', 'Status')))
         end
 
         def load_file!(path)
-          require_relative "#{::Hanami.root}/lib/#{path}"
+          require_relative "#{::Hanami.root}/#{path}"
         end
       end
     end
